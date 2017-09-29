4dd5cb2be5c62b0d73957f86547b1115d2c9f9f3
WangShuo1143368701 <1143368701@qq.com>
2017-09-29 10:10:01 +0800

    webRTC1.0

---
+++ app/src/main/java/com/ws/livegif/livegif/webrtc/WebRTCActivity.java
@@ -0,0 +1,15 @@
+package com.ws.livegif.livegif.webrtc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ws.livegif.livegif.R;
+
+public class WebRTCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_rtc);
+    }
+}
